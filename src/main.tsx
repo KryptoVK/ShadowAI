33d26b59642aa48b56046212a1043f9375a14e9c
KryptoVK <shadowsenderbot@gmail.com>
2024-12-06 02:57:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Performance monitoring
-const reportWebVitals = (metric: any) => {
-  // You can send the metrics to your analytics service here
-  console.log(metric);
-};
-
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
@@ -25,6 +19,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-// Report performance metrics
-reportWebVitals(window.performance);
