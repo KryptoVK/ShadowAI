8d9db73748315ddda32f803c4370133db4258ba6
KryptoVK <shadowsenderbot@gmail.com>
2024-12-06 01:11:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 10000,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  },
+  preview: {
+    host: true,
+    port: 10000,
+    strictPort: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
